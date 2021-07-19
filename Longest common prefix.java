10d7f03a62f7a278eaf7075b3e2b81d73e8defd3
NisthaGoyal <68862929+NisthaGoyal@users.noreply.github.com>
2021-07-19 23:37:32 +0530

    Create Longest common prefix.java

---
+++ Longest common prefix.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        
+        String longestCommonPrefix = strs[0];
+        
+        for (int i = 1; i < strs.length; i++) {
+            int j = 0;
+            String currentString = strs[i];
+            
+            while (j < longestCommonPrefix.length() && j < currentString.length() && longestCommonPrefix.charAt(j) == currentString.charAt(j)) {
+                j++;
+            }
+            
+            if (j == 0) {
+                return "";
+            }
+            
+            longestCommonPrefix = longestCommonPrefix.substring(0, j);
+        }
+        
+        return longestCommonPrefix;
+    }
+}
