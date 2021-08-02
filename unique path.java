04eb8c0a10ff1d30a4580c41b6d2de2ef80e32f0
NisthaGoyal <68862929+NisthaGoyal@users.noreply.github.com>
2021-08-02 23:52:35 +0530

    Create unique path.java

---
+++ unique path.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        
+        int[][] numberOfPaths = new int[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            numberOfPaths[i][0] = 1;
+        }
+        
+        for (int j = 0; j < n; j++) {
+            numberOfPaths[0][j] = 1;
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                numberOfPaths[i][j] = numberOfPaths[i - 1][j] + numberOfPaths[i][j - 1];
+            }
+        }
+        
+        return numberOfPaths[m - 1][n - 1];
+        
+        // Time Complexity: O(m * n)
+        // Space Complexity: O(m * n)        
+    }
+}
